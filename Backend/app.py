13c9448c0abbaeef957f375cf492d0caddf85c8d
Diego Iribe <diego.iribe@outlook.es>
2024-09-13 20:59:04 -0700

    agregando comando para cambiar el tamaño

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,27 @@
 from main import create_app, db
 import os
 
+def upgrade_database():
+    with db.engine.connect() as connection:
+        connection.execute("ALTER TABLE usuarios MODIFY COLUMN imagen LONGBLOB")
+        print("Columna 'imagen' modificada a LONGBLOB")
+
+# Crear la aplicación
 app = create_app()
 
+# Crear el contexto de la aplicación
 app.app_context().push()
 
 if __name__ == '__main__':
+    # Ejecutar la migración de la base de datos antes de iniciar el servidor
+    try:
+        upgrade_database()
+        print("Migración ejecutada exitosamente")
+    except Exception as e:
+        print(f"Error al ejecutar la migración: {str(e)}")
+
+    # Asegúrate de que las tablas existan
     db.create_all()
-    app.run(port = os.getenv("PORT"), debug=True)
+    
+    # Iniciar el servidor Flask
+    app.run(port=os.getenv("PORT"), debug=True)
